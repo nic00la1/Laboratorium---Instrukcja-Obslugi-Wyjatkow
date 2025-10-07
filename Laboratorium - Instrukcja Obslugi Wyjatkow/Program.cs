fe46affc7ca24bfdec3b5286eb9deeb8017a6eda
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-10-07 09:10:38 +0200

    test 1

--- Laboratorium - Instrukcja Obslugi Wyjatkow/Program.cs
+++ Laboratorium - Instrukcja Obslugi Wyjatkow/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿try
+{
+    int a = 10;
+    int b = 0;
+    int c = a / b; // Tutaj wystąpi DivideByZeroException
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Nie można dzielić przez zero! - Cholero!");
+}
