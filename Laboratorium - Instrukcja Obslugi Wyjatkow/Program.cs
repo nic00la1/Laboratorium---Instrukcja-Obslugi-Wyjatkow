726703267cf9c5178cc7e3f3c0d702ca21055964
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-10-07 09:15:43 +0200

    Throw

--- Laboratorium - Instrukcja Obslugi Wyjatkow/Program.cs
+++ Laboratorium - Instrukcja Obslugi Wyjatkow/Program.cs
@@ -8,3 +8,13 @@ catch (DivideByZeroException ex)
 {
     Console.WriteLine("Nie można dzielić przez zero! - Cholero!");
 }
+
+// Throw - Służy do ręcznego zgłoszenia wyjątku, np. kiedy chcesz wymusić
+// zakończenie działania funkcji w nieprawidłowej sytuacji
+
+int x = 10;
+
+if (x < 0)
+{
+    throw new ArgumentException("Liczba nie może być ujemna");
+}
